7f39707b5d6c4c885aa1dde5cb897557f59b4ace
Mason Bender <bender_mason@yahoo.com>
2018-02-26 10:08:24 -0700

    Added play, pause, volume, and scrubbing control

--- Audio Player/ViewController.swift
+++ Audio Player/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    let audioPath = Bundle.main.path(forResource: "HoldingOn", ofType: "mp3")
+    var timer = Timer()
+    
+    @objc func updatePositionSlider() {
+        positionSlider.value = Float(audioPlayer.currentTime)
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        audioPlayer.play()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updatePositionSlider), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        audioPlayer.pause()
+        timer.invalidate()
+    }
+    
+    // this is volume slider
+    @IBAction func volumeUpdated(_ sender: Any) {
+        audioPlayer.volume = slider.value
+    }
+    
+    // this is volume slider
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func positionSliderUpdated(_ sender: Any) {
+        audioPlayer.currentTime = TimeInterval(positionSlider.value)
+    }
+    
+    @IBOutlet weak var positionSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            positionSlider.maximumValue = Float(audioPlayer.duration)
+        } catch {
+            // error handling
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
